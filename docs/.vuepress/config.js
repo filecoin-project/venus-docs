25c62a639a3dd0953611ac0bdc680713325d02bb
eshon <1017762+eshon@users.noreply.github.com>
2021-01-08 20:47:20 +0100

    trying removing base url to fix gh-pages assets problem

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Venus Filecoin',
   description: 'Venus is a Go implementation of the Filecoin Distributed Storage Network.',
-  base: '/',
+  base: '',
   markdown: {
     config: md => {
       md.set({ linkify: true })
